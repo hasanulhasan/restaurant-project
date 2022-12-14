bfeaecf42c89f32b4258d6e76c9cdd3262710d99
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-12-15 01:13:21 +0600

    dish informatioin is showing

--- src/Components/Body/DishDetails.js
+++ src/Components/Body/DishDetails.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Card, CardBody, CardImg, CardText } from 'reactstrap';
 
-const DishDetails = () => {
+const DishDetails = (props) => {
   return (
     <div>
+      <Card style={{ marginTop: '10px' }}>
+        <CardImg top src={props.dish.image} alt={props.dish.name} />
+        <CardBody style={{ textAlign: 'left' }}></CardBody>
+        <CardText style={{ padding: '10px' }}>
+          <h4>{props.dish.name}</h4>
+          <p>{props.dish.description}</p>
+          <p>Price: ${props.dish.price}</p>
+        </CardText>
+
+      </Card>
 
     </div>
   );
--- src/Components/Body/Menu.js
+++ src/Components/Body/Menu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DISHES from '../../data/dishes/dishes';
+import DishDetails from './DishDetails';
 import MenuItem from './MenuItem';
 
 class Menu extends Component {
@@ -23,14 +24,23 @@ class Menu extends Component {
           onDishSelect={() => this.onDishSelect(item)}>
         </MenuItem>
       )
-    })
+    });
+
+    let dishDetail = null;
+    if (this.state.selectedDish != null) {
+      dishDetail =
+        <DishDetails
+          dish={this.state.selectedDish}
+        ></DishDetails>
+    }
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-6'>
             {menu}
-
-
+          </div>
+          <div className='col-6'>
+            {dishDetail}
           </div>
         </div>
       </div>
