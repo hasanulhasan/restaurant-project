49fe8dcab3d4580f2f0a5e73a631d97f7e4e981d
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-12-14 23:50:49 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MainComponent from './Components/MainComponent';
 function App() {
   return (
     <div className="App">
-      <h1>Restaurant Project is ready</h1>
       <MainComponent></MainComponent>
     </div>
   );
---
+++ src/Components/Body/Menu.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import DISHES from '../../data/dishes/Dishes';
+
+class Menu extends Component {
+  state = {
+    dishes: DISHES
+  }
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-6'>
+
+
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Menu;
