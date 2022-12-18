fd79f3d1b5ee0c06ebcd9cf0e1038e807cfa0acd
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-12-18 16:55:01 +0600

    redux successfully connected

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import MainComponent from './Components/MainComponent';
+import myStore from './redux/store';
 import router from './Routes/Router';
 
 function App() {
+  console.log('App.js:', myStore.getState());
   return (
     <div className="App">
-      <RouterProvider router={router}>
-        <MainComponent></MainComponent>
-      </RouterProvider>
+      <Provider store={myStore}>
+        <RouterProvider router={router}>
+          <MainComponent></MainComponent>
+        </RouterProvider>
+      </Provider>
     </div>
   );
 }
---
+++ src/redux/reducer.js
@@ -0,0 +1,12 @@
+import DISHES from "../data/dishes/dishes";
+import COMMENTS from "../data/dishes/comments";
+
+const initialState = {
+  dishes: DISHES,
+  comments: COMMENTS
+}
+
+export const Reducer = (state = initialState, action) => {
+  return state;
+
+}
